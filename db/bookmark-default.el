f7658b4548a95a8a16f0e12da4a4c203f6b5c19e
Kevin van Rooijen <kwrooijen@pm.me>
2025-12-05 16:46:05 +0100

    wip

--- db/bookmark-default.el
+++ db/bookmark-default.el
@@ -2,12 +2,15 @@
 ;;; This format is meant to be slightly human-readable;
 ;;; nevertheless, you probably don't want to edit it.
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
-(("ack"
-  (filename
-   . "~/Downloads/BACKUP/backup4/backup3/BACKUP2/BACKUP/backup2/")
-  (front-context-string . "ackup3/BACKUP2/B")
-  (rear-context-string . "BACKUP/backup4/b") (position . 46)
-  (last-modified 26928 35364 674510 0))
+(("Notes" (filename . "~/.notes.gpg") (front-context-string . #1="")
+  (rear-context-string . #1#) (position . 1)
+  (last-modified 26930 57346 126576 0))
+("ack"
+ (filename
+  . "~/Downloads/BACKUP/backup4/backup3/BACKUP2/BACKUP/backup2/")
+ (front-context-string . "ackup3/BACKUP2/B")
+ (rear-context-string . "BACKUP/backup4/b") (position . 46)
+ (last-modified 26928 35364 674510 0))
 ("Desktop" (filename . "~/Desktop/")
  (front-context-string . ".\n  drwxr-x---+ ")
  (rear-context-string . "6B Dec  2 21:58 ") (position . 77)
